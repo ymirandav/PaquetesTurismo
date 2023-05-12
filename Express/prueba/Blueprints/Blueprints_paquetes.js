afa7bfc297761fc459e6ca5989b40d8d7f6a1aac
André Cruces <ecrucesg@ulasalle.edu.pe>
2023-05-12 00:45:41 -0500

    Update

--- Express/prueba/Blueprints/Blueprints_paquetes.js
+++ Express/prueba/Blueprints/Blueprints_paquetes.js
@@ -1,42 +1,36 @@
 const express = require("express");
+const router = express.Router();
 
-const { Paquetes } = require("./Models/Model_paquetes");
+const { Paquetes } = require("../models/model_paquetes");
 
-const app = express();
+router.get("/paquetes", async (req, res) => {
+  const allPaquetes = await Paquetes.find();
+  return res.status(200).json(allPaquetes);
+});
 
-app.use(express.json());
+router.get("/paquetes/:id", async (req, res) => {
+  const { id } = req.params;
+  const paquete = await Paquetes.findById(id);
+  return res.status(200).json(paquete);
+});
 
-app.get("/paquetes", async (req, res) => {
-    const allPaquetes = await Paquetes.find();
-    return res.status(200).json(allPaquetes);
-    }
-);
+router.post("/paquetes", async (req, res) => {
+  const newPaquete = new Paquetes({ ...req.body });
+  const insertedPaquete = await newPaquete.save();
+  return res.status(201).json(insertedPaquete);
+});
 
-app.get("/paquetes/:id", async (req, res) => {
-    const { id } = req.params;
-    const paquete = await Paquetes.findById(id);
-    return res.status(200).json(paquete);
-    }
-);
+router.put("/paquetes/:id", async (req, res) => {
+  const { id } = req.params;
+  await Paquetes.updateOne({ _id: id }, req.body);
+  const updatedPaquete = await Paquetes.findById(id);
+  return res.status(200).json(updatedPaquete);
+});
 
-app.post("/paquetes", async (req, res) => {
-    const newPaquete = new Paquetes({ ...req.body });
-    const insertedPaquete = await newPaquete.save();
-    return res.status(201).json(insertedPaquete);
-    }
-);
+router.delete("/paquetes/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedPaquete = await Paquetes.findByIdAndDelete(id);
+  return res.status(200).json(deletedPaquete);
+});
 
-app.put("/paquetes/:id", async (req, res) => {
-    const { id } = req.params;
-    await Paquetes.updateOne({ id }, req.body);
-    const updatedPaquete = await Paquetes.findById(id);
-    return res.status(200).json(updatedPaquete);
-    }
-);
-
-app.delete("/paquetes/:id", async (req, res) => {
-    const { id } = req.params;
-    const deletedPaquete = await Paquetes.findByIdAndDelete(id);
-    return res.status(200).json(deletedPaquete);
-    }
-);
+module.exports = router;
--- Express/prueba/conection.js
+++ Express/prueba/conection.js
@@ -1,19 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const app = require("./app");
 
-const {Paquetes} = require("./Models/Model_paquetes");
-const {Usuarios} = require("./Models/Model_usuarios");
-const {Reservas} = require("./Models/Model_reservas");
-const {Turista} = require("./Models/Model_turista");
-
-const app = express();
+// const {Paquetes} = require("./models/model_paquetes");
+// const {Usuarios} = require("./models/model_usuarios");
+// const {Reservas} = require("./models/model_reservas");
+// const {Turistas} = require("./models/model_turistas");
 
 app.use(express.json());
+
+const PaquetesBlueprint = require("./Blueprints/Blueprints_paquetes");
+// const UsuariosBlueprint = require("./Blueprints/Blueprints_usuarios");
+// const ReservasBlueprint = require("./Blueprints/Blueprints_reservas");
+// const TuristasBlueprint = require("./Blueprints/Blueprints_turistas");
+
+app.use("/paquetes", PaquetesBlueprint);
+// app.use("/usuarios", UsuariosBlueprint);
+// app.use("/reservas", ReservasBlueprint);
+// app.use("/turistas", TuristasBlueprint);
 
 const start = async () => {
   try {
     await mongoose.connect(
-      "mongodb://localhost:27017/myDB"
+      "mongodb://127.0.0.1:27017/myDB"
     );
     app.listen(3000, () => console.log("Server started on port 3000"));
   } catch (error) {
